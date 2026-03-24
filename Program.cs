95355eeef46db7c60fac6a6df35a6d27bccfae8c
Pablo Bravo <pablo.bravo02@alumnos.ucn.cl>
2026-03-23 23:00:12 -0300

    fear(serilog): add and configure serilog

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddOpenApi();
 
+#region Loggin Configuration
+builder.Host.UseSerilog((context, services,configuration) =>configuration.ReadFrom.Configuration(context.Configuration).ReadFrom.Services(services));
+
+#endregion
 var app = builder.Build();
 
 app.MapOpenApi();
