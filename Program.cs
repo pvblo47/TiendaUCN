0baf02a39297f81be83a4d411eef146d4c2f0792
MathDev <matias.penailillo@alumnos.ucn.cl>
2026-04-14 16:44:36 -0400

    feat(infraestructure): set up DI, Resend y Settings base

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using Resend;
 using TiendaUCN.src.Infrastructure.Data;
+using TiendaUCN.src.Application.Services.Interfaces;
+using TiendaUCN.src.Application.Services.Implements;
+using TiendaUCN.src.Infrastructure.Repositories.Interfaces;
+using TiendaUCN.src.Infrastructure.Repositories.Implements;
 using DotNetEnv;
 
 Env.Load();
@@ -10,6 +15,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+//Configuracion de mapeadores
+//builder.Services.AddScoped<UserMapper>();
+
+//Configuracion de servicios y repositorios
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddScoped<IEmailService, EmailService>();
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+
 #region Loggin Configuration
 builder.Host.UseSerilog((context, services, configuration) => configuration
     .ReadFrom.Configuration(context.Configuration)
@@ -17,7 +34,7 @@ builder.Host.UseSerilog((context, services, configuration) => configuration
 #endregion
 
 #region Database Configuration
-Log.Information("Configurando base de datos SQLite");
+Serilog.Log.Information("Configurando base de datos SQLite");
 string connectionStringDB = Environment.GetEnvironmentVariable("DATA_BASE_URL")
 ?? throw new ArgumentNullException("DataBase name cannot be null or empty");
 
@@ -25,10 +42,21 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(connectionStringDB));
 #endregion
 
+#region Email Service Configuration
+Serilog.Log.Information("Configurando servicio de correo electrónico Resend");
+builder.Services.AddOptions();
+builder.Services.AddHttpClient<ResendClient>();
+builder.Services.Configure<ResendClientOptions>(o =>
+{
+    o.ApiToken = Environment.GetEnvironmentVariable("RESEND_API_KEY") ?? throw new ArgumentNullException("RESEND_API_KEY is not set");
+});
+builder.Services.AddTransient<IResend, ResendClient>();
+#endregion
+
 var app = builder.Build();
 
 #region Database Migration
-Log.Information("Aplicando migraciones a la base de datos");
+Serilog.Log.Information("Aplicando migraciones a la base de datos");
 using (var scope = app.Services.CreateScope())
 {
     await DataSeeder.Initialize(scope.ServiceProvider);
@@ -38,4 +66,3 @@ using (var scope = app.Services.CreateScope())
 app.MapOpenApi();
 
 app.Run();
-// a
