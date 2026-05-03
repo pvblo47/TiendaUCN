96f21dd53e2ca48e4c55b99447237030eed09673
MathDev <matias.penailillo@alumnos.ucn.cl>
2026-05-02 21:15:21 -0400

    feat(data): enable product brand an category sets in db context

--- src/Infrastructure/Data/DataContext.cs
+++ src/Infrastructure/Data/DataContext.cs
@@ -15,16 +15,18 @@ namespace TiendaUCN.src.Infrastructure.Data
         public DbSet<VerificationCode> VerificationCodes { get; set; } = null!;
 
         public DbSet<Image> Images { get; set; } = null!;
-        /*
+
         public DbSet<Product> Products { get; set; } = null!;
-        public DbSet<Image> Images { get; set; } = null!;
+        //public DbSet<Image> Images { get; set; } = null!;
         public DbSet<Brand> Brands { get; set; } = null!;
         public DbSet<Category> Categories { get; set; } = null!;
+        /*
         public DbSet<Cart> Carts { get; set; } = null!;
         public DbSet<CartItem> CartItems { get; set; } = null!;
         public DbSet<Order> Orders { get; set; } = null!;
         public DbSet<OrderItem> OrderItems { get; set; } = null!;
         */
+
         public DbSet<BlacklistedToken> BlacklistedTokens { get; set; } = null!;
     }
 }
